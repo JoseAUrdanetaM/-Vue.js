4889674bd612b2afeb5414904b7f8de460aaaf75
Jose <urdaneta.j@outlook.com>
2024-04-17 07:01:12 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import SearchInput from './components/SearchInput.vue'
+
+SearchInput
+</script>
+
+<template>
+  <!-- Date -->
+  <div class="text-center mb-6">
+    {{
+      new Date().toLocaleDateString('en-us', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })
+    }}
+  </div>
+
+  <!-- Search -->
+  <div><SearchInput /></div>
+</template>
